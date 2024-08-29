145fc93f3a544572b7ebf871400bdd626841fe0e
kazuki <88896808+KazukiGaran@users.noreply.github.com>
2024-08-29 14:27:40 +0900

    Screen Edit

--- Udemy/MemoApp/MemoApp2/src/app/auth/sign_up.tsx
+++ Udemy/MemoApp/MemoApp2/src/app/auth/sign_up.tsx
@@ -3,23 +3,29 @@ import {
   TouchableOpacity, StyleSheet
 } from 'react-native'
 
-import Header from '../../components/Header'
+import {Link, router} from 'expo-router'
 import Button from '../../components/Button'
+
+const handlePress = (): void =>{
+  //会員登録
+  router.push('/memo/list')
+}
 
 const Signup =(): JSX.Element => {
   return (
     <View style= {styles.container}>
-      <Header />
       <View style={styles.inner}>
         <Text style={styles.title}>Sign Up</Text>
         <TextInput style={styles.input} value='Email address' />
         <TextInput style={styles.input} value='Password' />
-        <Button label='submit'/>
+        <Button label='submit' onPress={handlePress}/>
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
-          <TouchableOpacity>
-          <Text style={styles.footerLink}>Log In.</Text>
-          </TouchableOpacity>
+          <Link href='/auth/log_in' asChild>
+            <TouchableOpacity>
+              <Text style={styles.footerLink}>Log In.</Text>
+            </TouchableOpacity>
+          </Link>
         </View>
       </View>
     </View>
